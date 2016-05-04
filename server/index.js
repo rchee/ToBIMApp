67afeb6d05f7d43081ef1cdb34fbf8bf1da5f84f
rchee <demarz@163.com>
2016-05-04 13:26:48 +0800

    服务端引入mockjs

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 var app = require('http').createServer(handler).listen(8080);
-var io = require('socket.io')(app);
+var io = require('socket.io')(app)
+var Mock = require('mockjs');
 
 var uuid = 1;
 //非常简单的 handler
@@ -17,13 +18,12 @@ io.on('connection', function (socket) {
   });
 
   setTimeout(function () {
-
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 50; i++) {
       socket.emit('newMessage', {
         id     : uuid++,
-        message: "Message" + (i + 1),//消息内容
-        date   : 1,//发送时间
-        from   : ((Math.random() * 5) >> 0),//来自谁（id）
+        message: "胡言乱语：" + Mock.Random.cparagraph(1, 4),//消息内容
+        date   : Date.now(),//发送时间
+        from   : ((Math.random() * 50) >> 0),//来自谁（id）
         to     : '000',//发给谁
       });
     }
