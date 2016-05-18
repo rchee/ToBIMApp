3e662c2bf06da23e6f1dbe1d40cfccd982e6c109
rchee <demarz@163.com>
2016-05-18 19:20:25 +0800

    持久化store

--- js/store/index.js
+++ js/store/index.js
@@ -1,12 +1,27 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers';
 import thunk from 'redux-thunk';
+import {persistStore, autoRehydrate} from 'redux-persist';
+import {AsyncStorage} from 'react-native';
+import immutableTransform from 'redux-persist-transform-immutable'
+/**
+ * 讲真，我不知道自己在写什么
+ */
+var isDebuggingInChrome = __DEV__ && !!window.navigator.userAgent;
 
-function configureStore() {
-  return createStore(
-    rootReducer,
-    applyMiddleware(thunk)
-  );
+var createAppStore = applyMiddleware(thunk)(createStore);
+
+function configureStore(onComplete:?() => void) {
+  // TODO(frantic): reconsider usage of redux-persist, maybe add cache breaker
+  const store = autoRehydrate()(createAppStore)(rootReducer);
+  persistStore(store, {
+    storage   : AsyncStorage,
+    transforms: [immutableTransform({})]
+  }, onComplete);
+  if (isDebuggingInChrome) {
+    window.store = store;
+  }
+  return store;
 }
 
 const store = configureStore();
